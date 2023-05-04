a3673ab3ea46c9842e5722a53853624ea7acff58
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-04 20:01:54 +0600

    footer created

--- src/Shared/Footer/Footer.jsx
+++ src/Shared/Footer/Footer.jsx
@@ -3,7 +3,18 @@ import React from 'react';
 const Footer = () => {
     return (
         <div>
-            <p className='text-center'>Mahmud uz zaman</p>
+            <div className='d-flex bg-dark  text-white-50 '>
+                <div>
+                    <h4>About Recipie Master</h4>
+                    <p className='w-25'>Recipies Master is a website . In this website your can see recipies details items. But the condition is first you need to register</p>
+                </div>
+                <div>
+                    <h4>You can find me on:</h4>
+                    <p>Email: zmahmud26@gmail.com</p>
+                    <p>Phone: 01759733296</p>
+                </div>
+            </div>
+            <p className='text-center bg-dark text-white-50  '>Copyright@ Mahmud uz zaman</p>
         </div>
     );
 };
