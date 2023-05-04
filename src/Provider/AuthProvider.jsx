c00c858fd4f220dce9fae79de25a056dbc920060
Md. Mahmud Uz Zaman <zmahmud26@gmail.com>
2023-05-04 12:31:21 +0600

    Logout done

--- src/Provider/AuthProvider.jsx
+++ src/Provider/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext } from 'react';
-import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { getAuth, createUserWithEmailAndPassword, onAuthStateChanged, signOut } from "firebase/auth";
 import app from '../firebase/firebase.config';
 
 
@@ -9,12 +9,27 @@ const auth = getAuth(app);
 
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState("{}");
 
-    const registerUser = ( email, password) => {
-    return createUserWithEmailAndPassword(auth, email, password)
+    const registerUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+    const logOut = () => {
+        return signOut(auth)
     }
 
-    const authInfo = {registerUser};
+
+    useEffect(() => {
+        const unSubcribe = onAuthStateChanged(auth, (loggedInUser) => {
+            setUser(loggedInUser)
+        });
+        return () => {
+            unSubcribe();
+        }
+    }, []);
+
+    const authInfo = { registerUser, user, logOut };
     return (
         <AuthContext.Provider value={authInfo}>
             {children}
--- src/Shared/Header/Header.jsx
+++ src/Shared/Header/Header.jsx
@@ -1,32 +1,48 @@
-import React from 'react';
-import { Button, Container, Nav,Navbar } from 'react-bootstrap';
+import React, { useContext } from 'react';
+import { Button, Container, Nav, Navbar } from 'react-bootstrap';
 import logo from '../../assets/blank-profile-picture.webp';
 import './Header.css'
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../Provider/AuthProvider';
 
 const Header = () => {
+    const { user, logOut } = useContext(AuthContext);
+    console.log(user?.email);
+
+    const handleLogout = () => {
+        logOut()
+        .then()
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
+
     return (
         <div >
             {/* <Container> */}
-                <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='Container'>
-                    <Container>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav">
-                            <h5>RECIPIES MASTER</h5>
-                            <Nav className="mx-auto">
-                                <Nav.Link href="/">Home</Nav.Link>
-                                <Nav.Link href="/blog">Blog</Nav.Link>
-                            </Nav>
-                            <Nav>
-                                <img className='pofile-img' src={logo} alt="" />
-                                <Nav.Link eventKey={2} >
-                                    <Link to='/login'><Button variant="secondary">Login</Button></Link>
+            <Navbar collapseOnSelect expand="lg" bg="light" variant="light" className='Container'>
+                <Container>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <h5>RECIPIES MASTER</h5>
+                        <Nav className="mx-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/blog">Blog</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <img className='pofile-img' src={logo} alt="" />
+                            <Nav.Link eventKey={2} >
+                                {user?.email ?
+                                    <Link><Button onClick={handleLogout} variant="secondary">LogOut</Button></Link> :
+                                    <Link to='/login'><Button variant="secondary">Login</Button></Link>}
 
-                                </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
+                                <Link>{user?.email}</Link>
+
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
 
             {/* </Container> */}
         </div>
